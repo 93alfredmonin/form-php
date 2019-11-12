79fab6a5d01e3e6c152c51a251be5b5f73f3d19f
skillup <sashadvortsov1@gmail.com>
2019-11-12 21:33:31 +0200

    V3

---
+++ server/views/login.php
@@ -0,0 +1,32 @@
+
+<form class="auth" action="">
+    <fieldset>
+        <legend>Авторизация</legend>
+        <div class="auth__row">
+            <label for="username">User name</label>
+            <input class="auth__text" type="text" id="username">
+            <i class="auth__error">Don't content (_, {}, -, $, 0-9) legth > 5 chars</i>
+        </div>
+
+        <div class="auth__row">
+            <label for="useremail">User e-mail</label>
+            <input class="auth__text" type="text" id="useremail">
+        </div>
+
+        <div class="auth__row">
+            <label for="userpass">User password</label>
+            <input class="auth__text" type="password" id="userpass">
+        </div>
+
+
+        <label class="auth__check">
+            Subscribe
+            <input type="checkbox" id="usersubscribe">
+        </label>
+
+        <div class="auth__row">
+            <button type="button" id="sendbtn" class="auth__btn" >Login</button>
+        </div>
+    </fieldset>
+</form>
+<script  src="/public/login.js"></script>
